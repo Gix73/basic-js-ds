b1161299557571782c549262e9b3a4c1b28d30da
Gix73 <gix73@yandex.ru>
2021-10-18 21:43:44 +0300

    feat: add linked-list task

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,7 +19,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function removeKFromList( l, k ) {
+  
+  if(l.value == k){
+    l.value = l.next
+  }  
+
+ let list = l;
+
+ while (list.next){
+   if(list.next.value == k){
+     list.next = list.next.next;
+   }else{
+    list = list.next;
+   }
+ }
+ return l;
 }
